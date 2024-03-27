f8ccce85c35c3155edbc803b720eb009a3775066
poozan <108912874+githubpglemec@users.noreply.github.com>
2024-03-27 20:16:21 +0545

    Add files via upload

---
+++ ActionDelegateNined.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ncit_hari
+{
+    internal class ActionDelegateNined
+    {
+        // Method to display a message
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine("Message: " + message);
+        }
+
+        // Method to display the current date and time
+        public static void DisplayDateTime()
+        {
+            Console.WriteLine("Current Date and Time: " + DateTime.Now);
+        }
+
+        // Main method
+        public static void Main(string[] args)
+        {
+            // Action delegate declaration
+            Action<string> displayMessageAction = DisplayMessage;
+            Action displayDateTimeAction = DisplayDateTime;
+
+            // Call the Action delegates
+            displayMessageAction("Hello, World!");
+            displayDateTimeAction();
+
+            // Additional information
+            Console.WriteLine("\nLab No.: 9");
+            Console.WriteLine("Name: Hari Prasad Gyawali");
+            Console.WriteLine("Roll No./Section: 06");
+        }
+    }
+}
